af47a4ec751a4095140868a5229c1b358c529c0e
Baneet Parmar <78154967+baneetparmar@users.noreply.github.com>
2021-10-09 18:43:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#imports
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
